1cca4bda6bf8e72c145aff30436ba74606e8cdae
Kelly <kellyaraujoo086@gmail.com>
2025-01-15 11:17:33 -0300

    Adicionando meu projeto na pasta Projeto1

---
+++ Projeto1/app.js
@@ -0,0 +1,12 @@
+alert("Boas Vindas ao Desafio de programação");
+let nome = "lua"
+let idade = 25
+let numeroDeVebdas = 50
+let saldoDisponivel = 1000
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+let nomee = prompt("Qual é o seu nome");
+let idadee = prompt("Digite sua idade");
+if(idade >= 18 ){
+ alert("Pode tirar a habilitação!")
+};
